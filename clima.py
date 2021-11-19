d50f573b003f7d4d6601d2440ae5192c7c15c3c4
Dannel <danneljt@gmail.com>
2021-11-19 21:41:06 +0100

    upload clima api find

--- clima.py
+++ clima.py
@@ -1,12 +1,17 @@
 from consumer_api import *
+from datetime import date
+
+today = date.today()
 
 
 def print_weather(city_weathers):
+    print("Clima para el dia" + str(today).center(50, "-"))
     print("Estado:", city_weathers['weather_state_name'])
     print("máxima:", city_weathers['max_temp'])
     print("Sensación térmica:", city_weathers['the_temp'])
     print("Humedad:", city_weathers['humidity'])
     print("Velocidad del viento y dirección:", city_weathers['wind_direction'])
+    print("**********************".center(50, "-"))
 
 
 user = "r"
@@ -25,7 +30,7 @@ while user.lower() != "q":
             print("Introduzca la ciudad".center(50, "-"))
             ciudad = input("Ciudad:")
             city_weathers = find_weather_by_city(ciudad)
-            print(print_weather(city_weathers))
+            print_weather(city_weathers[0]['consolidated_weather'].pop())
 
         if option_menu == "2":
             print("Introduzca las coordenadas (latitud y longitud)".center(50, "-"))
