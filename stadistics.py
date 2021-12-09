91765fafa29fff6a4ea87c5746ea072a7e0c79f7
Dannel <danneljt@gmail.com>
2021-12-09 20:34:10 +0100

    win lose method!

---
+++ stadistics.py
@@ -0,0 +1,42 @@
+class Stadistics:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def n(self):
+        return len(self.x)
+
+    @property
+    def avg_x(self):
+        return sum(self.x) / self.n
+
+    @property
+    def avg_y(self):
+        return sum(self.y) / self.n
+
+    @property
+    def variance_x(self):
+        x_media = self.avg_x
+        s1 = 0
+        for j in self.x:
+            d_pow = pow((j - x_media), 2)
+            s1 = s1 + d_pow
+        return s1/self.n
+
+    @property
+    def variance_y(self):
+        y_media = self.avg_y
+        s1 = 0
+        for j in self.y:
+            d_pow = pow((j - y_media), 2)
+            s1 = s1 + d_pow
+        return s1 / self.n
+
+    def __str__(self):
+        return f"X {self.x}\nY: {self.y}\nN: {self.n}"
+
+
+data_2 = Stadistics([2, 1, 3], [3, 5, 7])
+print(data_2.variance_x)
