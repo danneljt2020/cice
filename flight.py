ec2319ffa3e268b730695646c32aaef81c23a209
Dannel <danneljt@gmail.com>
2021-12-24 00:36:09 +0100

    estimate time!

--- flight.py
+++ flight.py
@@ -1,18 +1,31 @@
 import json
-from hashlib import sha256
-from random import random
+from datetime import datetime, timedelta
 
 
 class Flight:
-    def __init__(self, username, pwd):
-        self.username = username
-        pwd_e = sha256(pwd.encode()).hexdigest()
-        self.pwd = pwd_e  # encryp pass
+    def __init__(self, destination, origin, departure_hour, time_flight, utc):
+        self.origin = origin
+        self.destination = destination
+        self.departure_hour = departure_hour
+        self.time_flight = time_flight
+        self.utc = utc
+
+    def estimate_flight(self, origin_utc):
+        current_time = datetime.now()
+        hours_added_origin = timedelta(hours=origin_utc)
+        origin_time = current_time + hours_added_origin
+
+        hours_added_arrival = timedelta(hours=self.time_flight + self.utc[1])
+        estimate_time = origin_time + hours_added_arrival
+
+        return estimate_time
 
     @property
-    def user_dict(self):
+    def flight_dict(self):
         return {
-            "username": self.username,
-            "pwd": self.pwd
+            "origin": self.origin,
+            "destination": self.destination,
+            "departure_hour": self.departure_hour,
+            "time_flight": self.time_flight,
+            "utc": self.utc,
         }
-
--- main.py
+++ main.py
@@ -1,1 +1,80 @@
 import datetime as dt
+import json
+from flight import Flight
+
+DB_aereolina = "./aereolina.json"
+DB_flight = "./flight.json"
+
+
+def get_json_data(json_file):
+    with open(json_file, encoding="utf8") as file:
+        return json.load(file)
+
+
+def save_flight(data, json_file):
+    with open(json_file, "w", encoding="utf8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+def get_flight_by_origin(destiny):
+    origin_data = get_json_data(DB_aereolina)
+    flights = []
+    for flight in origin_data[destiny].items():
+        flights.append(flight)
+    return flights[2:]
+
+
+def get_all_origin():
+    origin_data = get_json_data(DB_aereolina)
+    destiny = []
+    for d in origin_data:
+        destiny.append(d)
+    return destiny
+
+
+user = ""
+while user != "q":
+    print("1. Comprar Vuelo")
+    print("2. Modificar Vuelo")
+    print("3. Cancelar Vuelo")
+
+    user = input("Seleccione: ")
+    if user == "1":
+        print("Seleccione el Origen")
+        for i, o in enumerate(get_all_origin()):
+            print(str(i + 1) + " --> " + o)
+
+        origin_opt = input("Seleccione: ")
+        origin_selected = get_all_origin()[int(origin_opt)-1]
+
+        print("Paises disponibles desde el origen " + origin_selected)
+
+        dat_destiny = get_flight_by_origin(origin_selected)
+        destinys = dat_destiny[1:]
+        utc = dat_destiny[0]
+
+        for i, d in enumerate(destinys):
+            print(str(i + 1) + " --> " + d[0])
+        destiny_opt = input("Seleccione: ")
+
+        flights_d = destinys[int(destiny_opt)-1]
+        departures = flights_d[1].get('departures')
+        destitation = flights_d[0]
+        flight_time = flights_d[1].get('flight_time')
+
+        print("Horarios de salida " + origin_selected + " hacia " + destitation)
+        for h, horary in enumerate(departures):
+            print(str(h + 1) + " --> " + horary)
+
+        horary_opt = input("Seleccione: ")
+        print(origin_selected)
+
+        flight_obj = Flight(destitation, origin_selected, departures[int(horary_opt)-1], flight_time, utc)
+
+        print(flight_obj.estimate_flight(-2))
+
+    elif user == "2":
+        print("Seleccione el vuelo a modificar")
+
+    elif user == "3":
+        pass
