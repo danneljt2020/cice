80b9b765112fbe2e2e0e650eaeb33fe78efbdb1e
Dannel <danneljt@gmail.com>
2022-01-11 20:07:11 +0100

    complete

---
+++ main_new.py
@@ -0,0 +1,36 @@
+import concurrent.futures
+import requests as req
+import threading
+
+base_url = 'https://restcountries.com/v3.1/all'
+
+
+# get all flags
+def get_all_flags():
+    list_flag = []
+    response = req.get(base_url)
+    data_response = response.json()
+    for f in data_response:
+        list_flag.append(f['flags']['png'])
+    return list_flag
+
+
+# Download flag country
+def save_flag(endpoint):
+    name = endpoint[-6:-4]
+    response = req.get(endpoint)
+    file = open("flags/flag_" + name + ".png", "wb")
+    file.write(response.content)
+    file.close()
+
+
+def download_flag():
+    flags = get_all_flags()
+    for endpoint in flags:
+        t1 = threading.Thread(target=save_flag, args=(endpoint,))
+        t1.start()
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    flags = get_all_flags()
+    executor.map(save_flag, flags)
